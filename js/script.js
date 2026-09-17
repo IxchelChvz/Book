3437d55e9be12a2846bc629a0633a1a593b1551c
Ixchel Waleska Chavez Linares <ix.chavez@nazaret.team>
2026-09-17 13:32:45 +0200

    Optimizacion de imagenes y mejora de estilos. Elimine index

--- js/script.js
+++ js/script.js
@@ -1,23 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-  // 1. Instanciar la librería indicando el elemento contenedor (#mi-libro)
   const pageFlip = new St.PageFlip(
     document.querySelector('.book'),
     {
-      width: 350,          // Ancho de CADA página en píxeles
-      height: 500,         // Alto de CADA página en píxeles
-      size: "fixed",       // Mantiene el tamaño fijo
+      width: 450,         
+      height: 600,        
+      size: "fixed",      
       minWidth: 300,
       maxWidth: 1000,
       minHeight: 400,
       maxHeight: 1200,
-      maxShadowOpacity: 0.5, // Intensa sombra en el pliegue
+      maxShadowOpacity: 1,
       showCover: true,
-      autoSize: true   // La 1ª página actúa como portada
+      autoSize: true   
     }
   );
 
-  // 2. Cargar las páginas desde el HTML leyendo todos los elementos con la clase .pagina
   pageFlip.loadFromHTML(document.querySelectorAll('.book-page'));
 
 });
